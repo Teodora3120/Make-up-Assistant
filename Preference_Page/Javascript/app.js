70c0119153241038a66db6658a2b8abf8ccd0e01
Teodora3120 <vestale.teodora00@gmail.com>
2022-04-09 14:04:29 +0300

    New changes

--- Preference_Page/Javascript/app.js
+++ Preference_Page/Javascript/app.js
@@ -1,29 +1,12 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-    
-    burger.addEventListener('click', ()=>{
-     // toggle nave
-    nav.classList.toggle('nav-active');
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-    // animatiomation of li
-    navLinks.forEach((link, index)=>{
-        if(link.style.animation){
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 2 + 0.3}s`;
-        }
-      
-    })
-    // burger animation
-    burger.classList.toggle('toggle');
-    });
-}
-
-const app = () =>{
-    navSlide();
-}
-
-app(); /* am ramas la minutul 22:10 link : https://www.youtube.com/watch?v=gXkqy0b4M5g */
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
