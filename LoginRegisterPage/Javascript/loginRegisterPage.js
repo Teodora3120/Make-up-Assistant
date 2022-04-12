754afa6e9ca557e8f618a99476fa74eb5d98f60d
SleepyShaman2134 <antoneaccalin@gmail.com>
2022-04-12 18:29:40 +0300

    Update Responsive+links

--- LoginRegisterPage/Javascript/loginRegisterPage.js
+++ LoginRegisterPage/Javascript/loginRegisterPage.js
@@ -1,1 +1,16 @@
 /* Aici va trebui mutat js ul din html */
+//SCRIPT MUTAT
+var x=document.getElementById("login");
+var y=document.getElementById("register");
+var z=document.getElementById("btn");
+
+function register(){
+  x.style.left="-100%";
+  y.style.left="33%";
+  z.style.left="110px";
+}
+function login(){
+  x.style.left="33%";
+  y.style.left="100%";
+  z.style.left="0px";
+}
