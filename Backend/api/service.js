26f745c7605c427d02f59aaeddd6d846072a305f
Teodora3120 <vestale.teodora00@gmail.com>
2022-05-14 20:51:04 +0300

    feat / mongoDB updates

--- Backend/api/service.js
+++ Backend/api/service.js
@@ -22,27 +22,29 @@ const service = async (req, res) => {
         res.end(JSON.stringify(todos));
         // handleSuccessResponse(todos);
     }
-    else if (req.url.match(/\/api\/todos\/(id[0-9]+)/) && req.method === "GET") {
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "GET") {
         try {
             // get id from url
             const id = req.url.split("/")[3];
+            console.log("URL ID: " + id);
             // get todo
             const todo = await new Todo().getTodo(id);
+            console.log("To do: " + todo);
             // set the status code and content-type
             res.writeHead(200, { "Content-Type": "application/json" });
             // send the data
             res.end(JSON.stringify(todo));
         } catch (error) {
-            // // set the status code and content-type
-            // res.writeHead(404, { "Content-Type": "application/json" });
-            // // send the error
-            // res.end(JSON.stringify({ message: error }));
-            handleErrorResponse(error);
+            // set the status code and content-type
+            res.writeHead(404, { "Content-Type": "application/json" });
+            // send the error
+            res.end(JSON.stringify({ message: error }));
+            //handleErrorResponse(error);
         }
     }
 
     // /api/todos/:id : DELETE
-    else if (req.url.match(/\/api\/todos\/(id[0-9]+)/) && req.method === "DELETE") {
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "DELETE") {
         try {
             // get the id from url
             const id = req.url.split("/")[3];
@@ -53,16 +55,16 @@ const service = async (req, res) => {
             // send the message
             res.end(JSON.stringify({ message }));
         } catch (error) {
-            // // set the status code and content-type
-            // res.writeHead(404, { "Content-Type": "application/json" });
-            // // send the error
-            // res.end(JSON.stringify({ message: error }));
-            handleErrorResponse(error);
+            // set the status code and content-type
+            res.writeHead(404, { "Content-Type": "application/json" });
+            // send the error
+            res.end(JSON.stringify({ message: error }));
+           // handleErrorResponse(error);
         }
     }
 
     // /api/todos/:id : UPDATE
-    else if (req.url.match(/\/api\/todos\/(id[0-9]+)/) && req.method === "PATCH") {
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "PATCH") {
         try {
             // get the id from the url
             const id = req.url.split("/")[3];
@@ -73,26 +75,26 @@ const service = async (req, res) => {
             // send the message
             res.end(JSON.stringify(updated_todo));
         } catch (error) {
-            // set the status code and content type
-            // res.writeHead(404, { "Content-Type": "application/json" });
-            // // send the error
-            // res.end(JSON.stringify({ message: error }));
-            handleErrorResponse(error);
+           // set the status code and content type
+            res.writeHead(404, { "Content-Type": "application/json" });
+            // send the error
+            res.end(JSON.stringify({ message: error }));
+           // handleErrorResponse(error);
         }
     } 
 }
 
-const handleSuccessResponse = (data) =>{
-    res.writeHead(200, { "Content-Type": "application/json" });
-    // send the message
-    res.end(JSON.stringify(data));
-}
+// const handleSuccessResponse = (data) =>{
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     // send the message
+//     res.end(JSON.stringify(data));
+// }
 
 
-const handleErrorResponse = (error) => {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    // send the error
-    res.end(JSON.stringify({ message : error }));
-}
+// const handleErrorResponse = (error) => {
+//     res.writeHead(404, { "Content-Type": "application/json" });
+//     // send the error
+//     res.end(JSON.stringify({ message : error }));
+// }
 
 module.exports = service;
